27f3ccc57e328425853a7936510f721ba49cf156
jiangkun1994 <jiangkun1994@163.com>
2018-05-30 13:14:47 +0200

    decrease the timer for check connection

--- in4073/in4073.h
+++ in4073/in4073.h
@@ -39,7 +39,7 @@ void run_filters_and_control();
 #define TIMER_PERIOD						50 	//50ms=20Hz (MAX 23bit, 4.6h)
 #define PANIC_MODE_PERIOD				200	//200ms
 #define TELEMETRY_TIMER_PERIOD 	500	//500ms
-#define CONNECTION_MODE_PERIOD	600 //600ms
+#define CONNECTION_MODE_PERIOD	400 //400ms
 #define MIN_PANIC_LOOPS					4
 void timers_init(void);
 uint32_t get_time_us(void);
